3699ca4e7b0c906d59f2a27d9ca2dfdb4bed4022
shad <shadow.mr96@gmail.com>
2016-10-06 16:36:56 +0500

    added visual test for GameImmutable to main() method

--- 15/15/Program.cs
+++ 15/15/Program.cs
@@ -10,13 +10,20 @@ namespace def
     {
         static void Main(string[] args)
         {
-            Game g = new Game(1, 2, 3, 0, 4, 5, 6, 7, 8);
-            g.PrintField();
-            g.Shift(1);
-            g.PrintField();
-            g.Shift(1);
+            //Game g = new Game(1, 2, 3, 0, 4, 5, 6, 7, 8);
+            //g.PrintField();
+            //g.Shift(1);
+            //g.PrintField();
+            //g.Shift(1);
+            //g.PrintField();
+            //g.Shift(4);
+            //g.PrintField();
+
+            GameImmutable g = new GameImmutable(1, 2, 3, 0, 4, 5, 6, 7, 8);
             g.PrintField();
             g.Shift(4);
+            g.PrintField();
+            g = g.Shift(4);
             g.PrintField();
             Console.ReadKey();
         }
