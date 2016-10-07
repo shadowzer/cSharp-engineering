4b7dae8526920a7d3eb3d2f9e2f50b82e51c11a5
shad <shadow.mr96@gmail.com>
2016-10-07 23:21:12 +0500

    Added structure "Cell" contains two coordinates : x & y

---
+++ 15/15/Cell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Def
+{
+    public struct Cell
+    {
+        public int X;
+        public int Y;
+    }
+}
