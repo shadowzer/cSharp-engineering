ffa08b6dddf7d9987b3ef1e5abaa8741d2e71313
shad <shadow.mr96@gmail.com>
2016-10-20 16:45:38 +0500

    fill main() method

---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class MyEngine { }
+    class MyEntity { }
+    class MyLogger { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var Processor = ProcessorBuilder.CreateEngine<MyEngine>().For<MyEntity>().With<MyLogger>();
+        }
+    }
+}
