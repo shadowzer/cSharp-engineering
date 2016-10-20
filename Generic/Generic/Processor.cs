81f3192beb764bf3719d3c1ecc02b60b4e99c9e6
shad <shadow.mr96@gmail.com>
2016-10-20 16:45:22 +0500

    added classes with filled methods() for build object var Processor = ProcessorBuilder.CreateEngine<MyEngine>().For<MyEntity>().With<MyLogger>();

---
+++ Generic/Generic/Processor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Processor<TEngine, TEntity, TLogger>
+    {
+
+    }
+
+    class ProcessorBuilder
+    {
+        public static Engine<TEngine> CreateEngine<TEngine>()
+        {
+            return new Engine<TEngine>();
+        }
+    }
+
+    class Engine<TEngine>
+    {
+        public EngineEntity<TEngine, TEntity> For<TEntity>()
+        {
+            return new EngineEntity<TEngine, TEntity>();
+        }
+    }
+
+    class EngineEntity<TEngine, TEntity>
+    {
+        public Processor<TEngine, TEntity, TLogger> With<TLogger>()
+        {
+            return new Processor<TEngine, TEntity, TLogger>();
+        }
+    }
+}
