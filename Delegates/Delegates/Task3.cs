24ec30716f7daba4c68f2587baab0c3a2112d2d3
shad <shadow.mr96@gmail.com>
2016-10-29 18:06:52 +0500

    deleted 2 interfaces

--- Delegates/Delegates/Task3.cs
+++ Delegates/Delegates/Task3.cs
@@ -7,18 +7,7 @@ using System.Threading.Tasks;
 
 namespace Delegates.Task3
 {
-    public interface IObservable
-    {
-        void AddObserver(Observer obs);
-        void RemoveObserver(Observer obs);
-    }
-
-    public interface IObserver
-    {
-        void Update(string obs);
-    }
-
-    public class Observer : IObserver
+    public class Observer
     {
         public void Update(string info)
         {
@@ -31,19 +20,19 @@ namespace Delegates.Task3
         public static void Log(string msg, string path = "log.txt")
         {
             FileStream fs = new FileStream(path, FileMode.Append);
-            string s = DateTime.Now.ToString() + "\t\t" + msg + "\n";
+            string s = DateTime.Now.ToString() + "\t\t" + msg;
             fs.Write(Encoding.ASCII.GetBytes(s), 0, s.Length);
         }
     }
 
-    public class Table : IObservable
+    public class Table
     {
         private List<Observer> Observers = new List<Observer>();
-        private int[,] TheTable;
+        private int[,] Table;
 
         public Table(Observer obs = null)
         {
-            TheTable = new int[0, 0];
+            Table = new int[0, 0];
             if (obs != null)
                 AddObserver(obs);
         }
@@ -52,7 +41,6 @@ namespace Delegates.Task3
         {
             Observers.Add(obs);
         }
-        // Action<Observer> AddObserver = obs => Observers.Add(obs);  ?товарищи проверяющие, как в этом анонимном делегате получить доступ к полю Observers?
 
         public void RemoveObserver(Observer obs)
         {
@@ -69,24 +57,24 @@ namespace Delegates.Task3
 
         private bool IsCellExist(int row, int column)
         {
-            return (row < TheTable.GetLength(0) || column < TheTable.GetLength(1)) ? true : false;
+            return (row < Table.GetLength(0) || column < Table.GetLength(1)) ? true : false;
         }
 
         private bool IsValidIndex(int idx, int dimension)
         {
-            return (idx < 0 || idx > TheTable.GetLength(dimension)) ? false : true;
+            return (idx < 0 || idx > Table.GetLength(dimension)) ? false : true;
         }
 
         private void UpdateTable(int[,] newTable)
         {
-            for (int i = 0; i < TheTable.GetLength(0); i++)
+            for (int i = 0; i < Table.GetLength(0); i++)
             {
-                for (int j = 0; j < TheTable.GetLength(1); j++)
+                for (int j = 0; j < Table.GetLength(1); j++)
                 {
-                    newTable[i, j] = TheTable[i, j];
+                    newTable[i, j] = Table[i, j];
                 }
             }
-            TheTable = newTable;
+            Table = newTable;
             
         }
 
@@ -94,7 +82,7 @@ namespace Delegates.Task3
         {
             if (!IsCellExist(row, column))
                 throw new Exception("Cell at row:" + row.ToString() + " column:" + column.ToString() + " does not exist");
-            TheTable[row, column] = value;
+            Table[row, column] = value;
             Notify("Puted " + value.ToString() + " to (" + row.ToString() + "; " + column.ToString() + ")");
         }
 
@@ -102,26 +90,23 @@ namespace Delegates.Task3
         {
             if (!IsValidIndex(rowIndex, 0))
                 throw new Exception("Row index is not valid");
-            var NewTable = new int[rowIndex + 1, TheTable.GetLength(1)];
+            var NewTable = new int[rowIndex + 1, Table.GetLength(1)];
             UpdateTable(NewTable);
-            Notify("Inserted row #" + (rowIndex + 1).ToString());
         }
 
         public void InsertColumn(int columnIndex) //- добавляет пустую колонку по указанному индексу
         {
             if (!IsValidIndex(columnIndex, 1))
                 throw new ArgumentException("Column index is not valid");
-            var NewTable = new int[TheTable.GetLength(0), columnIndex + 1];
+            var NewTable = new int[Table.GetLength(0), columnIndex + 1];
             UpdateTable(NewTable);
-            Notify("Inserted column #" + (columnIndex + 1).ToString());
         }
 
         public int Get(int row, int column) //- возвращает хранимое в таблице значение
         {
             if (!IsCellExist(row, column))
                 throw new Exception("Cell at row:" + row.ToString() + " column:" + column.ToString() + " does not exist");
-            Notify("Get " + TheTable[row, column].ToString() + " from (" + row.ToString() + "; " + column.ToString() + ")");
-            return TheTable[row, column];
+            return Table[row, column];
         }
 
     }
