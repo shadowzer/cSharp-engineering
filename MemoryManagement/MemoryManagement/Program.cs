a2b65989d1652ac3b996fc195af7c102b215ada1
shad <shadow.mr96@gmail.com>
2016-11-11 23:15:54 +0500

    initial commit for 1 task

---
+++ MemoryManagement/MemoryManagement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Timer();
+            using (timer.Start())
+            {
+                for (int i = 0; i < 10000000; ++i) ;
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+
+            using (timer.Continue())
+            {
+                for (int i = 0; i < 10000000; ++i) ;
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ MemoryManagement/MemoryManagement/Timer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryManagement
+{
+    class Timer : IDisposable
+    {
+        private Stopwatch timer = new Stopwatch();
+        private bool isDisposed = false;
+        public long ElapsedMilliseconds
+        {
+            get
+            {
+                return timer.ElapsedMilliseconds;
+            }
+        }
+
+        public Timer() { }
+
+        ~Timer()
+	    {
+		    Dispose(false);
+	    }
+
+	    public void Dispose()
+	    {
+		    Dispose(true);
+		    GC.SuppressFinalize(this);
+	    }
+
+	    protected virtual void Dispose(bool fromDisposeMethod)
+	    {
+		    if (!isDisposed)
+		    {
+			    if (fromDisposeMethod)
+			    {
+                    timer.Stop();
+			    }
+			    isDisposed = true;
+			}
+	    }
+
+        public Timer Start()
+        {
+            timer.Restart();
+            return this;
+        }
+
+        public Timer Continue()
+        {
+            timer.Start();
+            return this;
+        }
+    }
+}
