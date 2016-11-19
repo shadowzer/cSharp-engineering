b5dee106341c9d9938121c457f173e718a0ff8f0
shad <shadow.mr96@gmail.com>
2016-11-19 19:55:27 +0500

    Initial commit task 1

---
+++ Files/Files/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in CSV1.ReadCsv1())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
